95dca835ff7c7cec3885e5f5b0c81379799c7f66
Kyky <66506609+rizkyyz@users.noreply.github.com>
2020-12-23 22:42:08 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,6 +2,6 @@ print ("______________Tools Spammer__________")
 print ("=====================================")
 a = 12
 d = 10
-t = {a*d}
-print ("jadi hasilnya ,t ")
+t = a*d
+print ("jadi hasilnya : ",t)
  
