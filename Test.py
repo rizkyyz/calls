f236980b56b208bcdee6bc5aa03cde4cf7f19298
Kyky <66506609+rizkyyz@users.noreply.github.com>
2020-12-23 23:03:31 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,11 @@
-print ("______________Tools perkalian__________")
+print ("______________Tools Tambah__________")
 print ("=====================================")
 panjang = raw_input("masukin angka pertama :")
 lebar = input("masukin angka ke dua :")
-luas = panjang * lebar
+luasi = panjang + lebar
 print(luas)
+print("~~~~~~~~")
+print ("______________Tools kurang__________")
+print ("=====================================")
+luasy = panjang - lebar
+print(luasy)
